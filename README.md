f6fe802c1a696ee0cff979a8300b40ce4059fdf7
Cecilj24 <clangba@yahoo.com>
2023-04-27 22:51:22 -0700

    added data to weather app

---
+++ README.md
@@ -0,0 +1,65 @@
+ <Weather-project>
+
+## Description
+
+This project is a weather application generator that generates any 5 day weather forecast of any city and it also shows the temperature, windspeed and humidity. The data of the components shown on the page is showed in the console log. The data is also stored in the local storage for future use. 
+
+
+N/A
+
+## Installation
+
+My project can be used by all browsers provided that the HTML link is used within the browser.
+
+## Usage
+
+N/A
+
+## Credits
+Tutors
+Erick Hirsch
+Andres Jimenez
+
+## License
+
+MIT License
+
+Copyright (c) 2023 Cecilj24
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+## Badges
+
+n/a
+
+## Features
+
+CSS Stylesheet
+Javascript file
+Links
+UI will scroll to the section correspondant to the link
+
+
+## How to Contribute
+
+N/A
+
+## Tests
+When the HTML is open, it will go to a page labeled "weather project" and it will open the page up and above is a search bar to search any city within the united states and it will display the date, temperatue, windspeed and humidity within the five blue boxes below. 
+
---
+++ script.js
@@ -0,0 +1,71 @@
+var weatherKey = "d0c19ceab7f74c33d5bcc2a50f5f0296";
+var weatherEndPoint = "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=" + weatherKey;
+var monday = document.querySelector(".day1");
+var tuesday = document.querySelector(".day2");
+var wednesday = document.querySelector(".day3");
+var thursday = document.querySelector(".day4");
+var friday = document.querySelector(".day5");
+var temp1 = document.querySelector(".temp1");
+var element = document.querySelectorAll('.element');
+var windspeed = document.querySelectorAll('.windSpeed');
+var windspeed1 = document.querySelector(".windspeed1");
+var humidity = document.querySelectorAll('.humidity');
+var humidity1 = document.querySelector(".humidity1");
+var search = document.querySelector(".search");
+var searchBar = document.querySelector(".searchBar");
+var cityName = document.querySelector(".cityName");
+
+
+
+async function getWeatherByCity(cityName) {
+    var endResult = weatherEndPoint + "&q=" + cityName;
+    var respond = await fetch(endResult);
+    var weather = await respond.json();
+
+    // console.log(weather);
+    // console.log(weather.main.humidity)
+    // console.log(weather.main.temp)
+    // console.log(weather.wind.speed)
+    temp1.textContent = 'temperature: ' + weather.main.temp
+    windspeed1.textContent = 'windspeed:' + weather.wind.speed + "mph"
+    humidity1.textContent = "humidity:" + weather.main.humidity
+    var lat = weather.coord.lat
+    var lon = weather.coord.lon
+    // pull  city name form data and save it in an array of searches in local storage *remember only stings can be saved to local storage so you will need you JSON.stringify() when saving and JSON.parse() when retrieving 
+    // localstorage.setItem(key, value)  localstorage.getItem(key)
+    forcastWeather(lat, lon);
+}
+
+search.addEventListener("click", (event) => {
+    if (event.type === "click") {
+        console.log(searchBar.value)
+        getWeatherByCity(searchBar.value);
+    }
+})
+
+async function forcastWeather(lat, lon) {
+    var respond = await fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=' + weatherKey);
+    var data = await respond.json();
+
+    console.log(data)
+    var forecast = []
+    for (let i = 7; i < data.list.length; i += 8) {
+
+        console.log(data.list[i]);
+
+        forecast.push(data.list[i])
+    }
+    for (let i = 0; i < element.length; i++) {
+        const children = element[i].children;
+        children[0].textContent = forecast[i].dt_txt
+        children[1].textContent =forecast[i].main.temp + "\u00B0"
+        children[2].textContent =forecast[i].wind.speed + "mph"
+        children[3].textContent =forecast[i].main.humidity + "%"
+
+        
+    }
+}
+
+function fiveDayForecast () {
+
+}
