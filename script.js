df3f8eb1b957b8518d5fbf94772477ce6cac17da
Cecilj24 <clangba@yahoo.com>
2023-12-13 17:47:14 -0800

    updated the UI design

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
 var weatherKey = "d0c19ceab7f74c33d5bcc2a50f5f0296";
 var weatherEndPoint = "https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=" + weatherKey;
-var monday = document.querySelector(".day1");
-var tuesday = document.querySelector(".day2");
-var wednesday = document.querySelector(".day3");
-var thursday = document.querySelector(".day4");
-var friday = document.querySelector(".day5");
 var temp = document.querySelector(".temp");
 var icons = document.querySelectorAll('.icons');
 var windspeed = document.querySelectorAll('.windspeed');
 var humidity = document.querySelectorAll('.humidity');
-var search = document.querySelector(".search");
+var search = document.querySelector(".search-btn");
 var searchBar = document.querySelector(".searchBar");
 var cityName = document.querySelector(".cityName");
+var card = document.querySelectorAll(".card")
 
 
 
 async function getWeatherByCity(cityName) {
     var endResult = weatherEndPoint + "&q=" + cityName;
     var respond = await fetch(endResult);
-    var weather = await respond.json();
+    var weather = await respond.json(); // retrieves data from API CALL
 
     console.log(weather);
-    console.log(weather.main.humidity);
-    console.log(weather.main.temp);
-    console.log(weather.wind.speed);
-    temp.textContent = 'temperature: ' + weather.main.temp;
-    windspeed.textContent = 'windspeed:' + weather.wind.speed + "mph";
-    humidity.textContent = "humidity:" + weather.main.humidity;
+    // console.log(weather.main.humidity);
+    // console.log(weather.main.temp);
+    // console.log(weather.wind.speed);
+    // temp.textContent = 'temperature: ' + weather.main.temp;
+    // windspeed.textContent = 'windspeed:' + weather.wind.speed + "mph";
+    // humidity.textContent = "humidity:" + weather.main.humidity;
     var lat = weather.coord.lat;
     var lon = weather.coord.lon;
     // pull  city name form data and save it in an array of searches in local storage *remember only stings can be saved to local storage so you will need you JSON.stringify() when saving and JSON.parse() when retrieving 
     // localstorage.setItem(key, value)  localstorage.getItem(key)
     forcastWeather(lat, lon);
 }
+
+
 
 search.addEventListener("click", (event) => {
     if (event.type === "click") {
@@ -53,12 +51,12 @@ async function forcastWeather(lat, lon) {
 
         forecast.push(data.list[i])
     }
-    for (let i = 0; i < icons.length; i++) {
-        const children = icons[i].children;
+    for (let i = 0; i < card.length; i++) {
+        const children = card[i].children;
         children[0].textContent = forecast[i].dt_txt;
-        children[1].textContent = forecast[i].main.temp + "\u00B0";
-        children[2].textContent = forecast[i].wind.speed + "mph";
-        children[3].textContent = forecast[i].main.humidity + "%";
+        children[2].textContent = forecast[i].main.temp + "\u00B0";
+        children[3].textContent = forecast[i].wind.speed + "mph";
+        children[4].textContent = forecast[i].main.humidity + "%";
 
 
     }
